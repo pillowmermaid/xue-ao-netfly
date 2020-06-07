57a87da18ca1c79dccd2edb29f2ba98486de4ebc
brettapitz <53789094+brettapitz@users.noreply.github.com>
2020-06-07 15:59:17 -0400

    Began work on navbar
    Navbar styling and responsiveness are mostly done.
    
    Still todo: onscroll transition, fixing class change on resizing browser.
    
    I grabbed Xue's original colors, which suck against the current homepage background.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,15 +35,12 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar is-transparent is-fixed-top"
         role="navigation"
         aria-label="main-navigation"
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
@@ -59,35 +56,12 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
+            <Link className="navbar-item" activeClassName="is-active" to="/">
+              Portfolio
+            </Link>
+            <Link className="navbar-item" activeClassName="is-active" to="/about">
+              About Me
+            </Link>
           </div>
         </div>
       </nav>
